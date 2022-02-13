1bd749725ba4a9a384e6173db96527776be8d397
Kaleab Solomon <87313721+baby2733@users.noreply.github.com>
2022-02-14 01:35:51 +0300

    Add files via upload

---
+++ mike website/index.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+const lightt = document.querySelector(".lightt");
+const dark = document.querySelector(".dark");
+const menu = document.querySelector(".menu");
+const bi = document.querySelector(".bi")
+const header = document.querySelector(".header");
+
+
+bi.addEventListener("click", function(){
+    menu.classList.toggle("see");
+    header.getElementsByClassName.dis
+    
+   
+;})
+
+
+
+dark.onclick = function(){
+    body.classList.replace('light','dark');
+}
+
+lightt.onclick = function(){
+    body.classList.replace('dark','light')
+}
+
+
+
+
